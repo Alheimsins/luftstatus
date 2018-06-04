1b1c066387700730fe356b8f4b6a0c7cfde2f750
Jonas Enge <jonas.enge@gmail.com>
2018-06-04 19:28:24 +0200

    Genesis commit

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,6 @@
 import { geolocated } from 'react-geolocated'
 import { Component } from 'react'
-import ReactMapGL, { Marker } from 'react-map-gl'
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
 import { FaCircle } from 'react-icons/lib/fa'
 const token = 'pk.eyJ1IjoibWFjY3liZXIiLCJhIjoiY2ppMGR4MGszMDA4ZzNwczdlbDRocmwyMSJ9.ey1URzpaVGR2MkBfhLoSrQ'
 
@@ -20,21 +20,39 @@ class Map extends Component {
     }
   }
 
+  renderPopup() {
+    const { popupInfo } = this.state
+    return popupInfo && (
+      <Popup tipSize={5}
+        anchor='top'
+        latitude={59.2663054}
+        longitude={9.2235048}
+        offsetLeft={-12}
+        onClose={() => this.setState({popupInfo: false})}
+      >
+        <div width='240px'>HELLO</div>
+      </Popup>
+    )
+  }
+
   render () {
-    const { latitude, longitude, viewport } = this.state
+    const { latitude, longitude, viewport, show } = this.state
     const lat = parseFloat(latitude) || 59.2663054
     const long = parseFloat(longitude) || 9.2235048
-    console.log(long)
     return (
       <div style={{ width: '100%', height: '100%' }}>
         <ReactMapGL
           {...viewport}
           mapboxApiAccessToken={token}
           onViewportChange={(viewport) => this.setState({viewport})}
+          mapStyle='mapbox://styles/mapbox/dark-v9'
         >
-          <Marker latitude={lat} longitude={long} offsetLeft={-20} offsetTop={-10}>
-            <FaCircle style={{ color: '#6ee86e' }} />
+          <Marker latitude={lat} longitude={long} offsetLeft={-20} offsetTop={-10} captureClick={false}>
+            <span onClick={() => this.setState({popupInfo: true})}>
+              <FaCircle style={{ color: '#6ee86e' }} />
+            </span>
           </Marker>
+          {this.renderPopup()}
         </ReactMapGL>
       </div>
     )
